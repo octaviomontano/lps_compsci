806f127f6bb2b6c0090f042f5215d52b5f05752b
Octavio Montano <rc.montano.octavio@leadps.org>
2016-11-16 17:47:32 +0000

    w/ challenge

--- class_samples/3-5_looppractice/multiply.py
+++ class_samples/3-5_looppractice/multiply.py
@@ -10,5 +10,5 @@ while multiple < ceiling:
 	print(str(count) + ' times ' + str(number) + ' equals ' + str(math))
 	multiple = count * number
 	count += 1
-print'There are', count ,'multiples of', number,'up to', str(ceiling) + '! Have a nice day!'
+print'There are', count ,'integer multiples of', number,'that are less than', str(ceiling) + '. Have a nice day!'
 
